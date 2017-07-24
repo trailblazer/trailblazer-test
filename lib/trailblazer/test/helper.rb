9b000adfe98a70d76bb6df2687cae637f9f9d1ed
Nick Sutterer <apotonick@gmail.com>
2017-07-24 14:54:40 +0200

    introduce #call and #factory helper.

--- lib/trailblazer/test/helper.rb
+++ lib/trailblazer/test/helper.rb
@@ -7,9 +7,16 @@ module Trailblazer::Test
 
       def call!(operation_class, args, raise_on_failure:false)
         operation_class.(*args).tap do |result|
-          raise "[Trailblazer-test] #{operation_class} returned an invalid state." if raise_on_failure
+          raise OperationFailedError, "[Trailblazer-test #factory] #{operation_class} returned an invalid state." if raise_on_failure && !result.success?
         end
       end
+
+      def factory(*args)
+        call!(*args, raise_on_failure: true)
+      end
     end # Operation
+  end
+
+  class OperationFailedError < RuntimeError
   end
 end
--- test/helper_test.rb
+++ test/helper_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class HelperTest < Minitest::Spec
-  Result    = Struct.new(:input)
-  Operation = ->(*args) { Result.new( args ) }
+  Result    = Struct.new(:input, :success) do
+    def success?; self.success; end
+  end
+  Operation = ->(*args) { Result.new( args, true ) }
+  FailingOperation = ->(*args) { Result.new( args, false ) }
 
   class Test < Minitest::Spec
     include Trailblazer::Test::Helper::Operation
@@ -14,10 +17,23 @@ class HelperTest < Minitest::Spec
     it do
       call HelperTest::Operation, { a: 1 }, { b: 2}
     end
+
+    it do
+      model = factory( HelperTest::Operation, {} )
+    end
+
+    it do
+      assert_raises Trailblazer::Test::OperationFailedError do
+        factory( HelperTest::FailingOperation, {} )
+      end
+    end
   end
 
   it { assert_equal [{}], Test.new(:a).test_0001_anonymous.input }
   it { assert_equal [{:a=>1}, {:b=>2}], Test.new(:a).test_0002_anonymous.input }
 
-
+  # returns result.
+  it { assert_equal %{#<struct HelperTest::Result input=[], success=true>}, Test.new(:a).test_0003_anonymous.inspect }
+  # raises error
+  it { assert_instance_of Trailblazer::Test::OperationFailedError, Test.new(:a).test_0004_anonymous }
 end

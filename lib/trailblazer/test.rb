1cd5edd8e1a8a13cacbe2b5c069037bcc975b2a7
Myron Marston <myron.marston@gmail.com>
2017-07-29 20:59:53 +0700

    Proof of concept of using rspec-expectations.

--- lib/trailblazer/test.rb
+++ lib/trailblazer/test.rb
@@ -6,6 +6,8 @@ module Trailblazer
   end
 end
 
+require "minitest/spec"
+require "rspec/expectations/minitest_integration"
 require "trailblazer/test/assertions"
 require "trailblazer/test/helper"
 # require "trailblazer/test/operation/assertions"
--- test/assertions_test.rb
+++ test/assertions_test.rb
@@ -1,44 +1,14 @@
 require "test_helper"
 
 class AssertionsTest < Minitest::Spec
-  class Test < Minitest::Spec
-    def initialize(*)
-      super
-      @_assertions = []
-    end
-
-    def assert_equal(a, b, msg)
-      @_assertions << [a, b, msg]
-    end
-
-    def assert(a, msg)
-      @_assertions << [a, msg]
-      # super
-    end
-
-    def call
-      run
-      @_assertions
-    end
-
-    include Trailblazer::Test::Assertions
-  end
-
+  include Trailblazer::Test::Assertions
 
   it do
-    test =
-      Class.new(Test) do
-        let(:model) { Struct.new(:title, :band).new("__Timebomb__", "__Rancid__") }
+    model = Struct.new(:title, :band).new("__Timebomb__", "__Rancid__")
 
-        #:exp-eq
-        it do
-          assert_exposes model, title: "Timebomb", band: "Rancid"
-        end
-        #:exp-eq end
-      end.
-      new(:test_0001_anonymous) # Note: this has to be that name, otherwise the test case won't be run!
-
-    assert_equal [["Timebomb", "__Timebomb__", "Property [title] mismatch"], ["Rancid", "__Rancid__", "Property [band] mismatch"]], test.()
+    expect {
+      assert_exposes model, title: "Timebomb", band: "Rancid"
+    }.to raise_error(a_string_including 'expected #<struct title="__Timebomb__", band="__Rancid__"> to have attributes')
   end
 
   class Song
@@ -47,57 +17,12 @@ class AssertionsTest < Minitest::Spec
   end
 
   it do
-    test =
-      Class.new(Test) do
-        let(:model) { Song.new }
+    model = Song.new
 
-        #:exp-proc
-        it do
-          assert_exposes model,
-            title: "Timebomb",
-            band:  ->(actual:, **) { actual.size > 3 }
-        end
-        #:exp-proc end
-      end.
-      new(:test_0001_anonymous) # Note: this has to be that name, otherwise the test case won't be run!
-
-    assert_equal [["Timebomb", "__Timebomb__", "Property [title] mismatch"], [true, "Actual: \"__Rancid__\"."]], test.()
-  end
-
-  # reader: :[]
-  it do
-    test =
-      Class.new(Test) do
-        let(:model) { { title: "__Timebomb__", band: "__Rancid__" } }
-
-        #:exp-reader-hash
-        it do
-          assert_exposes model, { title: "Timebomb", band:  "Rancid" }, reader: :[]
-        end
-        #:exp-reader-hash end
-      end.
-      new(:test_0001_anonymous) # Note: this has to be that name, otherwise the test case won't be run!
-
-    assert_equal [["Timebomb", "__Timebomb__", "Property [title] mismatch"], ["Rancid", "__Rancid__", "Property [band] mismatch"]], test.()
-  end
-
-  it do
-    test =
-      Class.new(Test) do
-        class Song
-          def get(name); name == :title ? "__Timebomb__": "__Rancid__" end
-        end
-
-        let(:model) { Song.new }
-
-        #:exp-reader-get
-        it do
-          assert_exposes model, { title: "Timebomb", band:  "Rancid" }, reader: :get
-        end
-        #:exp-reader-get end
-      end.
-      new(:test_0001_anonymous) # Note: this has to be that name, otherwise the test case won't be run!
-
-    assert_equal [["Timebomb", "__Timebomb__", "Property [title] mismatch"], ["Rancid", "__Rancid__", "Property [band] mismatch"]], test.()
+    expect {
+      assert_exposes model,
+        title: "Timebomb",
+        band:  ->(actual) { actual.size > 3 }
+    }.to raise_error(a_string_including '-:title => "Timebomb"', '+:title => "__Timebomb__"')
   end
 end

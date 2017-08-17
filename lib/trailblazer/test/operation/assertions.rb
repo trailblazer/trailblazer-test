040b29c9bc9b9de37516408800974303b3409589
Nick Sutterer <apotonick@gmail.com>
2017-08-17 21:16:50 +0200

    restructure.

--- lib/trailblazer/test/operation/assertions.rb
+++ lib/trailblazer/test/operation/assertions.rb
@@ -1,15 +1,19 @@
 module Trailblazer::Test::Operation
   module Assertions
     def assert_passes(operation_class, params, expected_attributes, &block)
-      default_params = params_valid
+      default_params = params_valid # FIXME.
       default_attributes = attributes_valid # FIXME.
 
-      result = operation_class.( default_params.merge(params) )
 
-      assert_result_passes(result, expected_attributes, &block)
+      params = default_params.merge(params)
+
+      assert_passes_with_model(operation_class, params, expected_attributes, &block)
     end
 
-    def assert_result_passes(result, expected_attributes={}, &block) # TODO: test expected_attributes default param and explicit!
+    # @private
+    def assert_passes_with_model(operation_class, params, expected_attributes, &block) # TODO: test expected_attributes default param and explicit!
+      result = operation_class.( params )
+
       assert_equal true, result.success?
 
       return yield result if block_given?  # DISCUSS: result or model?

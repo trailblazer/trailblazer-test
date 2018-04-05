5db2b3059014b77af544e63a36bf51225812ea56
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2018-04-05 18:30:27 +0800

    Rubocop introduced
    Just few "style fix"

--- lib/trailblazer/test/assertions.rb
+++ lib/trailblazer/test/assertions.rb
@@ -4,17 +4,18 @@ module Trailblazer
     # Evaluate value if it's a lambda, and let the caller know whether we need an
     # assert_equal or an assert.
     def self.expected(asserted, value, actual)
-      value.is_a?(Proc) ? [ value.(actual: actual, asserted: asserted), false ] : [ value, true ]
+      value.is_a?(Proc) ? [value.(actual: actual, asserted: asserted), false] : [value, true]
     end
 
     # Read the actual value from the asserted object (e.g. a model).
     def self.actual(asserted, reader, name)
-       reader ? asserted.send(reader, name) : asserted.send(name)
+      reader ? asserted.send(reader, name) : asserted.send(name)
     end
 
     module Assertions
       module_function
-        # tuples = defaults.merge(overrides) # FIXME: merge with above!
+
+      # tuples = defaults.merge(overrides) # FIXME: merge with above!
 
       # Test if all `tuples` values on `asserted` match the expected values.
       # @param asserted Object Object that exposes attributes to test
@@ -25,7 +26,7 @@ module Trailblazer
           actual          = Test.actual(asserted, reader, k)
           expected, is_eq = Test.expected(asserted, v, actual)
 
-          is_eq ? assert_equal( expected, actual, "Property [#{k}] mismatch" ) : assert(expected, "Actual: #{actual.inspect}.")
+          is_eq ? assert_equal(expected, actual, "Property [#{k}] mismatch") : assert(expected, "Actual: #{actual.inspect}.")
         end
       end # TODO: test err msgs!
     end

d3de294dd47ef73a53d3ceb6da9a375f81d11b45
Nick Sutterer <apotonick@gmail.com>
2021-11-18 22:23:39 +0100

    simplify internals.

--- lib/trailblazer/test/operation/assertions.rb
+++ lib/trailblazer/test/operation/assertions.rb
@@ -2,25 +2,25 @@ require "hashie"
 
 module Trailblazer::Test::Operation
   module Assertions
-    # @needs default_params
-    # @needs default_options
-    # @needs expected_attrs
 
-    def params(default_params: self.default_params, deep_merge: true, **new_params)
-      {params: merge_for(default_params, new_params, deep_merge)}
+    # Defaults so tests run without tweaking (almost).
+    def self.included(includer)
+      includer.let(:operation)     { raise "`let(:operation) { ... }` is missing" }
+      includer.let(:key_in_params) { false }
     end
 
-    def ctx(new_params, default_options: self.default_options, deep_merge: true, **options)
-      new_params  = merge_for(params[:params], new_params, deep_merge)
-      new_options = merge_for(default_options, options, deep_merge)
-
-      {params: new_params, **new_options}
-    end
-
-    def assert_pass(operation_class, operation_inputs, expected_attributes, default_attributes: expected_attrs, deep_merge: true, &block)
+    def assert_pass(params_fragment, expected_attributes, use_wtf=false, operation: self.operation, key_in_params: self.key_in_params, default_attributes: expected_attrs, default_ctx: self.default_ctx, deep_merge: true, &block)
       expected_attributes = merge_for(default_attributes, expected_attributes, deep_merge)
 
-      assert_pass_with_model(operation_class, operation_inputs, expected_model_attributes: expected_attributes, &block)
+      # If {:key_in_params} is given, key the {params_fragment} with it, e.g. {params: {transaction: {.. params_fragment ..}}}
+      merge_with_ctx = key_in_params ? {params: {key_in_params => params_fragment}} : {params: params_fragment}
+
+      ctx = merge_for(default_ctx, merge_with_ctx, true)
+
+      kws = {}
+      kws[:invoke_method] = :wtf? if use_wtf # TODO: same for #assert_fail !
+
+      assert_pass_with_model(operation, ctx, expected_model_attributes: expected_attributes, **kws, &block)
     end
 
     def assert_fail(operation_class, operation_inputs, expected_errors: nil, contract_name: "default", &block)
@@ -29,16 +29,20 @@ module Trailblazer::Test::Operation
 
     # @private
     # TODO: test expected_attributes default param and explicit!
-    def assert_pass_with_model(operation_class, operation_inputs, expected_model_attributes: {}, &user_block)
-      _assert_call(operation_class, operation_inputs, user_block: user_block) do |result|
-        assert_equal true, result.success?
+    def assert_pass_with_model(operation_class, ctx, expected_model_attributes: {}, **kws, &user_block)
+      _assert_call(operation_class, ctx, user_block: user_block, **kws) do |result|
+
+        errors = result[:"contract.default"].errors.messages.inspect
+        colored_errors = %{\e[33m#{errors}\e[0m}
+        assert_equal( true, result.success?, %{{#{operation_class}} failed: #{colored_errors}}) # FIXME: only if contract's there!
+
         assert_exposes(_model(result), expected_model_attributes)
       end
     end
 
     # @private
-    def assert_fail_with_model(operation_class, operation_inputs, expected_errors: nil, contract_name: raise, &user_block)
-      _assert_call(operation_class, operation_inputs, user_block: user_block) do |result|
+    def assert_fail_with_model(operation_class, ctx, expected_errors: nil, contract_name: raise, &user_block)
+      _assert_call(operation_class, ctx, user_block: user_block) do |result|
         assert_equal true, result.failure?
 
         raise ExpectedErrorsTypeError, "expected_errors has to be an Array" unless expected_errors.is_a?(Array)
@@ -51,8 +55,8 @@ module Trailblazer::Test::Operation
     end
 
     # @private
-    def _assert_call(operation_class, operation_inputs, user_block: raise)
-      result = _call_operation(operation_class, operation_inputs)
+    def _assert_call(operation_class, ctx, user_block: raise, **kws)
+      result = _call_operation(operation_class, ctx, **kws)
 
       return user_block.call(result) if user_block # DISCUSS: result or model?
 
@@ -77,8 +81,8 @@ module Trailblazer::Test::Operation
     end
 
     # @private
-    def _call_operation(operation_class, operation_inputs)
-      operation_class.(operation_inputs)
+    def _call_operation(operation_class, ctx, invoke_method: :call, **)
+      operation_class.send(invoke_method, ctx)
     end
 
     # @private

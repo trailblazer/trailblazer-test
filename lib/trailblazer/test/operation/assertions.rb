7e1e2d5c72667c6ffe199e0dfb0e89a77062c370
Nick Sutterer <apotonick@gmail.com>
2021-11-19 10:36:04 +0100

    move all kws processing to `#normalize_kws`.

--- lib/trailblazer/test/operation/assertions.rb
+++ lib/trailblazer/test/operation/assertions.rb
@@ -9,40 +9,59 @@ module Trailblazer::Test::Operation
       includer.let(:key_in_params) { false }
     end
 
-    def assert_pass(params_fragment, expected_attributes, use_wtf=false, operation: self.operation, key_in_params: self.key_in_params, default_attributes: expected_attrs, default_ctx: self.default_ctx, deep_merge: true, &block)
+    def assert_pass(params_fragment, expected_attributes, use_wtf=false, default_attributes: expected_attrs, deep_merge: true, **kws, &block)
       expected_attributes = merge_for(default_attributes, expected_attributes, deep_merge)
 
+      kws = _normalize_kws(use_wtf, block, **kws)
+      ctx = _ctx_for_params_fragment(params_fragment, **kws)
+
+
+      assert_pass_with_model(ctx, expected_model_attributes: expected_attributes, **kws)
+    end
+
+    def assert_fail(params_fragment, expected_errors, use_wtf=false, contract_name: "default", **kws, &block)
+      kws = _normalize_kws(use_wtf, block, **kws)
+      ctx = _ctx_for_params_fragment(params_fragment, **kws)
+
+      assert_fail_with_model(ctx, expected_errors: expected_errors, contract_name: contract_name, **kws)
+    end
+
+    private def _ctx_for_params_fragment(params_fragment, key_in_params:, default_ctx:, **)
       # If {:key_in_params} is given, key the {params_fragment} with it, e.g. {params: {transaction: {.. params_fragment ..}}}
       merge_with_ctx = key_in_params ? {params: {key_in_params => params_fragment}} : {params: params_fragment}
 
       ctx = merge_for(default_ctx, merge_with_ctx, true)
-
-      kws = {}
-      kws[:invoke_method] = :wtf? if use_wtf # TODO: same for #assert_fail !
-
-      assert_pass_with_model(operation, ctx, expected_model_attributes: expected_attributes, **kws, &block)
     end
 
-    def assert_fail(operation_class, operation_inputs, expected_errors: nil, contract_name: "default", &block)
-      assert_fail_with_model(operation_class, operation_inputs, expected_errors: expected_errors, contract_name: contract_name, &block)
+    private def _normalize_kws(use_wtf, block, operation: self.operation, key_in_params: self.key_in_params, default_ctx: self.default_ctx)
+      kws = {
+        user_block:     block,
+        operation:      operation,
+        default_ctx:    default_ctx,
+        key_in_params:  key_in_params,
+      }
+
+      kws[:invoke_method] = :wtf? if use_wtf
+
+      return kws
     end
 
     # @private
     # TODO: test expected_attributes default param and explicit!
-    def assert_pass_with_model(operation_class, ctx, expected_model_attributes: {}, **kws, &user_block)
-      _assert_call(operation_class, ctx, user_block: user_block, **kws) do |result|
+    def assert_pass_with_model(ctx, operation:, expected_model_attributes: {}, **kws, &user_block)
+      _assert_call(operation, ctx, **kws) do |result|
 
         errors = result[:"contract.default"].errors.messages.inspect
         colored_errors = %{\e[33m#{errors}\e[0m}
-        assert_equal( true, result.success?, %{{#{operation_class}} failed: #{colored_errors}}) # FIXME: only if contract's there!
+        assert_equal( true, result.success?, %{{#{operation}} failed: #{colored_errors}}) # FIXME: only if contract's there!
 
         assert_exposes(_model(result), expected_model_attributes)
       end
     end
 
     # @private
-    def assert_fail_with_model(operation_class, ctx, expected_errors: nil, contract_name: raise, &user_block)
-      _assert_call(operation_class, ctx, user_block: user_block) do |result|
+    def assert_fail_with_model(ctx, operation:, expected_errors: nil, contract_name: raise, **kws, &user_block)
+      _assert_call(operation, ctx, **kws) do |result|
         assert_equal true, result.failure?
 
         raise ExpectedErrorsTypeError, "expected_errors has to be an Array" unless expected_errors.is_a?(Array)
@@ -50,7 +69,7 @@ module Trailblazer::Test::Operation
         # only test _if_ errors are present, not the content.
         errors = result["contract.#{contract_name}"].errors.messages # TODO: this will soon change with the operation Errors object.
 
-        assert_equal expected_errors.sort, errors.keys.sort
+        assert_equal expected_errors.sort, errors.keys.sort, "Contract errors: #{errors}"
       end
     end
 
